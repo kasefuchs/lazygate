a927598fd60a414ea1ad2e35399b7cb4a3180823
LuckCrafter <jeremy@gottlieb.cyou>
2025-05-20 12:28:20 +0200

    feat: dont refresh token if not needed (reduse traffic)

--- pkg/provider/pufferpanel/client.go
+++ pkg/provider/pufferpanel/client.go
@@ -7,12 +7,15 @@ import (
 	"net/url"
 	"os"
 	"strings"
+	"time"
 )
 
 type Client struct {
 	baseUrl       string
 	client_id     string
 	client_secret string
+	token         OAuth2Token
+	expires       time.Time
 }
 
 type OAuth2Token struct {
@@ -33,25 +36,39 @@ func NewClient() (*Client, error) {
 	}, nil
 }
 
-func (client *Client) getToken() (*OAuth2Token, error) {
+func (client *Client) newToken() error {
 	var token OAuth2Token
 	data := url.Values{}
+	_time := time.Now()
 
 	data.Set("client_id", client.client_id)
 	data.Set("client_secret", client.client_secret)
 	data.Set("grant_type", "client_credentials")
 	resp, err := http.PostForm(client.baseUrl+"oauth2/token", data)
 	if err != nil {
-		return nil, err
+		return err
 	}
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return nil, err
+		return err
 	}
 	err = json.Unmarshal(body, &token)
 	if err != nil {
-		return nil, err
+		return err
 	}
+	client.expires = _time.Add(time.Second * time.Duration(token.Expires_in))
+	client.token = token
+	return nil
+}
+
+func (client *Client) getToken() (*OAuth2Token, error) {
+	if time.Now().Compare(client.expires) > 0 {
+		err := client.newToken()
+		if err != nil {
+			return nil, err
+		}
+	}
+	token := client.token
 	return &token, nil
 }
